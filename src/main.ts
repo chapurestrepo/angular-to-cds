7126d37759a7c9a32a0b57578ed7684724d156e4
chapurestrepo <danielcgrafico@gmail.com>
2024-02-19 17:54:39 +0100

    cds en angular

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,21 @@ import 'zone.js';
   selector: 'app-root',
   standalone: true,
   template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
+
+  <div style="display:flex; flex-direction:column; gap:100px; padding:20px; align-items:center;">
+
+  <button class='cds-button cds-button--secondary'>
+    <span class='icon ico-refresh'></span>
+    <span class='cds-button__text'>Refresh</span>
+  </button>
+
+
+  <div class="cds-tooltip" style="display:flex; margin:auto;">
+  -hover me-
+  <span class="cds-tooltip__content cds-tooltip--help cds-tooltip--top cds-tooltip--small">Content of the tooltip.</span>
+  </div>
+  </div>
+  
   `,
 })
 export class App {
